b9f3eaa9e4bc0a2cbdb93ab4756af56bab217a66
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-11-28 04:29:53 -0800

    get_precision

--- get_precision.c
+++ get_precision.c
@@ -21,7 +21,7 @@ int get_precision(const char *format, int *i, va_list list)
 			precision *= 10;
 			precision += format[curr_i] - '0';
 		}
-		else if (format[curr_i] == '*')
+		if (format[curr_i] == '*')
 		{
 			curr_i++;
 			precision = va_arg(list, int);
