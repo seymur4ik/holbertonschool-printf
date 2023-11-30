e184bdfe7b7392e3c0267213c7f0f2395a874d5c
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-11-30 09:00:22 -0800

    update

--- get_precision2.c
+++ get_precision2.c
@@ -5,27 +5,27 @@
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: List of arguments.
- * 
+ *
  * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
 		int curr_i = *i + 1;
 			int precision = -1;
-
-				if (format[curr_i] != '.')
+			
+			if (format[curr_i] != '.')
 							return (precision);
 
 					precision = 0;
-
-						for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+					
+					for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 								{
-											if (is_digit(format[curr_i]))
+									if (is_digit(format[curr_i]))
 														{
 																		precision *= 10;
 																					precision += format[curr_i] - '0';
 																							}
-													else if (format[curr_i] == '*')
+									else if (format[curr_i] == '*')
 																{
 																				curr_i++;
 																							precision = va_arg(list, int);
