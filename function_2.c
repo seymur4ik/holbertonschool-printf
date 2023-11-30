5a729195ada59719ca64aea7611069fc34fb0505
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-11-30 08:12:50 -0800

    update

--- function_2.c
+++ function_2.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- *  * print_pointer - Prints the value of a pointer variable
- *   * @types: List a of arguments
- *    * @buffer: Buffer array to handle print
- *     * @flags: Calculates active flags
- *      * @width: get width
- *       * @precision: Precision specification
- *        * @size: Size specifier
- *         * Return: Number of chars printed.
- *          */
+ * print_pointer - Prints the value of a pointer variable
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags: Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed.
+ */
 int print_pointer(va_list types, char buffer[],
 			int flags, int width, int precision, int size)
 {
