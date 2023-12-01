58ecc23599df7a37579ec93d59db47c8c4dda343
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-12-01 07:37:54 -0800

    !

--- functions2.c
+++ functions2.c
@@ -95,13 +95,14 @@ int print_non_printable(va_list types, char buffer[],
  *    * @buffer: Buffer array to handle print
  *     * @flags: Calculates active flags
  *      * @width: get width
- *       * 
+ *       * @precision: Precision specification 
  *        * @size: Size specifier
  *         * Return: Numbers of chars printed
  *          */
 int print_reverse(va_list types, char buffer[],
-			int flags, int width, int size)
+			int flags, int width, int precision int size)
 {
+	(void) precision;
 		char *str;
 			int i, count = 0;
 
