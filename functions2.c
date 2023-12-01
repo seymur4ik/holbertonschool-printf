9a60bcd0b8c8874318def45fc183c2933f3fc998
ilaahe04 <ilaha.hybtv04@gmail.com>
2023-12-01 08:13:30 -0800

    2

--- functions2.c
+++ functions2.c
@@ -99,7 +99,7 @@ int print_non_printable(va_list types, char buffer[],
  *        * @size: Size specifier
  *         * Return: Numbers of chars printed
  *          */
-int print_reverse(va_list types, char buffer[],
+void print_reverse(va_list types, char buffer[],
 			int flags, int width, int precision, int size)
 {
 		char *str;
